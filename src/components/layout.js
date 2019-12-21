fbef8c206590bee2d6032f891c8d16472bd73c66
Harshal Patil <harshal.rp@gmail.com>
2019-12-21 16:26:51 +0530

    Icon System RAW

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,9 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
+import { useStaticQuery, graphql } from 'gatsby';
+import React from 'react';
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import { Header } from './Header';
 
-import Header from "./header"
-import "./layout.css"
+import './layout.css';
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -21,32 +14,23 @@ const Layout = ({ children }) => {
         }
       }
     }
-  `)
+  `);
 
   return (
-    <>
+    <div>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
-}
+      <main>
+        {children}
+      </main>
+      <footer>
+        © {new Date().getFullYear()}, Built with
+        {` `}
+        <a href='https://www.gatsbyjs.org'>Gatsby</a>
+      </footer>
+    </div>
 
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
+  );
+};
 
-export default Layout
+
+export default Layout;
